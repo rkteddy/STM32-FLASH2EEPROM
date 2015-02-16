1c81f5b5c4bac7f4dde6e4f06604cc6fd8d778be
Runkai Zheng <838474413@qq.com>
2015-02-16 11:00:37 +0800

    Delay funcs

---
+++ eeprom.c
@@ -0,0 +1,15 @@
+#include "eeprom.h"
+#include "stm32f10x.h"
+
+void delay_us(u32 n)
+{
+		u8 j;
+		while(n--)
+		for(j = 0; j < 10; j++);
+}
+
+void delay_ms(u32 n)
+{
+		while(n--)
+		delay_us(1000);
+}
